96ac9a232d598a79979d564898992822bc0e0507
willkernel <willkerneljc@gmail.com>
2018-04-11 00:37:34 +0800

    lambda

---
+++ Function/cmake-build-debug/CMakeFiles/Function.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Function.dir/main.cpp.obj"
+  "Function.pdb"
+  "Function.exe"
+  "Function.exe.manifest"
+  "libFunction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Function/main.cpp
+++ Function/main.cpp
@@ -9,7 +9,7 @@ void fun_swap_value(int, int);
 
 void fun_swap_point(int *, int *);
 
-void fun_swap_cite(int, int);
+void fun_swap_cite(int &x, int &y);
 
 void fun_pptr();
 
@@ -22,6 +22,56 @@ void getSeconds(unsigned long *par);
 void getAvg(int *ptr, int size);
 
 int *getRandom();
+
+/**C++11 提供了对匿名函数的支持,称为 Lambda 函数
+ * Lambda 表达式本质上与函数声明非常类似。Lambda 表达式具体形式
+ * [capture](parameters) mutable ->return-type{statement}
+ * [capture]：捕捉列表。捕捉列表总是出现在 lambda 表达式的开始处。事实上，[] 是 lambda 引出符。编译器根据该引出符判断接下来的代码是否是 lambda 函数。捕捉列表能够捕捉上下文中的变量供 lambda 函数使用。
+ (parameters)：参数列表。与普通函数的参数列表一致。如果不需要参数传递，则可以连同括号 () 一起省略。
+ mutable：mutable 修饰符。默认情况下，lambda 函数总是一个 const 函数，mutable 可以取消其常量性。在使用该修饰符时，参数列表不可省略（即使参数为空）。
+ ->return_type：返回类型。用追踪返回类型形式声明函数的返回类型。出于方便，不需要返回值的时候也可以连同符号 -> 一起省略。此外，在返回类型明确的情况下，也可以省略该部分，让编译器对返回类型进行推导。
+ {statement}：函数体。内容与普通函数一样，不过除了可以使用参数之外，还可以使用所有捕获的变量
+ *
+ * 在Lambda表达式内可以访问当前作用域的变量，这是Lambda表达式的闭包（Closure）行为。
+ * 与JavaScript闭包不同，C++变量传递有传值和传引用的区别。可以通过前面的[]来指定：
+ * []      // 沒有定义任何变量。使用未定义变量会引发错误。
+   [x, &y] // x以传值方式传入（默认），y以引用方式传入。
+   [&]     // 任何被使用到的外部变量都隐式地以引用方式加以引用。
+   [=]     // 任何被使用到的外部变量都隐式地以传值方式加以引用。
+   [&, x]  // x显式地以传值方式加以引用。其余变量以引用方式加以引用。
+   [=, &z] // z显式地以引用方式加以引用。其余变量以传值方式加以引用。
+ * */
+class LambdaTest {
+public:
+    void lambda() {
+
+        int global_x = 0;
+        //没有返回值
+        [&global_x] { ++global_x; };
+        //返回true，false
+        [](int x, int y) { return x > y; };
+        //指定返回int
+        [](int x, int y) -> int {
+            int z = x + y;
+            return z + x;
+        };
+        [this]() { this->someFunc(); }();
+        [] {};//最简单的lambda表达式
+    }
+
+    /**函数调用运算符的重载方法是const属性的
+     * 有时候，你想改动传值方式捕获的值，那么就要使用mutable*/
+    void someFunc() {
+        int x = 10;
+
+        auto add_x = [x](int a) mutable {
+            x *= 2;
+            return a + x;
+        };  // 复制捕捉x
+
+        cout << add_x(10) << endl; // 输出 30
+    }
+};
 
 int main() {
     int a = 100;//实际整型变量
@@ -90,14 +140,22 @@ int main() {
     cout << "a=" << a << endl;
     cout << "b=" << b << endl;
     fun_swap_value(a, b);
-
+    cout << "fun_swap_value a=" << a << endl;
+    cout << "fun_swap_value b=" << b << endl;
 
     /** 调用函数来交换值
     * &a 表示指向 a 的指针，即变量 a 的地址
     * &b 表示指向 b 的指针，即变量 b 的地址
     */
     fun_swap_point(&a, &b);
+    cout << "fun_swap_point a=" << a << endl;
+    cout << "fun_swap_point b=" << b << endl;
+
+    fun_swap_cite(a, b);
+    cout << "fun_swap_cite a=" << a << endl;
+    cout << "fun_swap_cite b=" << b << endl;
     cout << "---------" << endl;
+
     /**传递指针给函数，声明函数参数为指针类型*/
     unsigned long sec;
     getSeconds(&sec);
@@ -115,6 +173,10 @@ int main() {
     }
 
     cout << "---------" << endl;
+
+    LambdaTest t;
+    t.lambda();
+    cout << "----END-----" << endl;
     return 0;
 }
 
@@ -203,14 +265,12 @@ int max(int a, int b) {
 }
 
 /**
- * 传值调用
+ * 传值调用,默认情况下，C++ 使用传值调用来传递参数
  * 实际参数的参数值传值给形式参数，调用不会影响实际参数*/
 void fun_swap_value(int a, int b) {
     int temp = a;
     a = b;
     b = temp;
-    cout << "fun_swap_value a=" << a << endl;
-    cout << "fun_swap_value b=" << b << endl;
 }
 
 /**指针调用
@@ -221,10 +281,14 @@ void fun_swap_point(int *a, int *b) {
     temp = *a; // 保存地址 x 的值
     *a = *b; //把 y 赋值给 x
     *b = temp; //把 x 赋值给 y
-    cout << "fun_swap_point a=" << a << endl;
-    cout << "fun_swap_point b=" << b << endl;
 }
 
-void fun_swap_cite(int) {
-
+/**引用调用
+ * 把引用的地址复制给形式参数。在函数内，
+ * 该引用用于访问调用中要用到的实际参数，修改形式参数会影响实际参数*/
+void fun_swap_cite(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
