3902589d0f6f3604f0a6062e02782b33304503d7
willkernel <willkerneljc@gmail.com>
2018-04-20 00:41:41 +0800

    date time cout cerr clog

---
+++ DateTimeDemo/cmake-build-debug/CMakeFiles/DateTimeDemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DateTimeDemo.dir/main.cpp.obj"
+  "DateTimeDemo.pdb"
+  "DateTimeDemo.exe"
+  "DateTimeDemo.exe.manifest"
+  "libDateTimeDemo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DateTimeDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- DateTimeDemo/main.cpp
+++ DateTimeDemo/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <ctime>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // 基于当前系统的当前日期/时间
+    time_t now = time(nullptr);
+    cout << "now " << now << endl;
+    // 把 now 转换为字符串形式
+    char *dt = ctime(&now);
+    cout << "local date/time " << dt << endl;
+    // 把 now 转换为 tm 结构
+    tm *gmtm = gmtime(&now);
+    dt = asctime(gmtm);
+    cout << "UTC date/time " << dt << endl;
+
+    tm *ltm = localtime(&now);
+    cout << "Y: " << ltm->tm_year << endl;
+    cout << "M: " << ltm->tm_mon << endl;
+    cout << "D: " << ltm->tm_mday << endl;
+    cout << "Time: " << ltm->tm_hour << ":";
+    cout << " " << ltm->tm_min << ":";
+    cout << " " << ltm->tm_sec << endl;
+
+    char name[50];
+    int age;
+    cout << "your name age :";
+    cin >> name >> age;
+    cout << " name is " << name << ", age is " << age << endl;
+
+    char str[] = "exec";
+
+    cerr << "error " << str << endl;
+    wcerr << "error " << str << endl;
+
+    clog << "error " << str << endl;
+    wclog << "error " << str << endl;
     return 0;
 }
+
